539efeb62f2028a5da4cc7b7b4f85ed1062c5879
Ben Wilson <ben.wilson@ert.com>
2018-08-13 15:00:49 -0700

    Creation

---
+++ README.md
@@ -0,0 +1,8 @@
+# new-react-app
+
+Starter template for React.JS
+
+Based on:
+
+<https://medium.com/@kayodeniyi/simplest-react-app-setup-a74277b99e43>
+<https://webpack.js.org/guides/getting-started/>
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/app'),
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
+      {test: /(\.css)$/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  plugins: [
+      new CopyWebpackPlugin([
+          { from: 'public' }
+      ])
+  ]
+};
